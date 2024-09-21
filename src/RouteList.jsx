79524cd94d226a37d0179c9beb30356ae63d5f04
danx <rushn24@gmail.com>
2024-09-20 21:51:34 -0400

    added NotFound component

--- src/RouteList.jsx
+++ src/RouteList.jsx
@@ -6,6 +6,7 @@ import Contact from './Contact';
 import BlogHome from './BlogHome';
 import NewBlogForm from './NewBlogForm';
 import Post from './Post';
+import NotFound from './NotFound';
 
 const RoutesList = () => {
     return (
@@ -17,6 +18,7 @@ const RoutesList = () => {
                 <Route path="/blog" element={<BlogHome />}>BlogHome</Route>
                 <Route path="/blog/new" element={<NewBlogForm />}>NewBlogForm</Route>
                 <Route path="/blog/:slug" element={<Post />}>Post</Route>
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </>
     );
